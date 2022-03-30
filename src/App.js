26a47e13960d721178a469f3021ce4641607cb07
Eric Janero <ejanerop97@gmail.com>
2022-03-29 23:11:36 -0400

    Add routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import {Routes, Route} from "react-router-dom";
+import Header from "./components/header/header.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
     </div>
-  );
+);
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/hats" element={<HatsPage />} />
+                    <Route path="/shop" element={<ShopPage />} />
+                </Routes>
+            </div>
+        );
+    }
 }
 
 export default App;
